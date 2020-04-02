8f1c91a932d203d5750f8ed45e3c9c70fa12cbc8
Ethan <ejr@HHIH34NK.hsd1.ca.comcast.net>
2020-04-02 15:10:05 -0700

    Co-authored-by: OscarMoss21 <oscartmoss@gmail.com>

---
+++ js/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    textSize(32);
+    text('welcome to HABIT TRACKER', 30, 60);
+    fill(0, 142, 153);
+  }
